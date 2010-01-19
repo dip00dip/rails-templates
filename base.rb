7afe27bed08ad6dfe0e3726611b21d362c59217b
Diego Rossini Vieira <eu@diegorv.com>
2010-01-19 19:01:57 -0200

    my changes

--- base.rb
+++ base.rb
@@ -1,22 +1,98 @@
-if yes?("Do you want to use RSpec for testing?")
-  plugin "rspec", :git => "git://github.com/dchelimsky/rspec.git"
-  plugin "rspec-rails", :git => "git://github.com/dchelimsky/rspec-rails.git"
-  generate :rspec
+# set some constants
+APP_DIRNAME = File.basename(root)
+APP_NAME = APP_DIRNAME.classify
+APP_DOMAIN = "#{APP_DIRNAME}.info"
+
+# Set up git repository
+git :init
+
+# remove all useless public files
+run "rmdir tmp/{pids,sessions,sockets,cache}"
+run "rm README"
+run "rm log/*.log"
+run "rm public/index.html"
+run "rm public/javascripts/*"
+run "rm public/favicon.ico"
+run "rm public/images/rails.png"
+run "rm public/robots.txt"
+run "rm -rf test"
+run "rm -rf doc"
+run "rm config/locales/*"
+
+# keep empty dirs
+run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitkeep \\;")
+
+# basic .gitignore file
+file '.gitignore', 
+%q{log/*.log
+log/*.pid
+db/*.db
+db/*.sqlite3
+db/schema.rb
+tmp/**/*
+.DS_Store
+Thumbs.db
+doc/api
+doc/app
+config/database.yml
+autotest_result.html
+coverage
+public/javascripts/*_packaged.js
+public/stylesheets/*_packaged.css
+public/attachments
+}
+
+# Configure gems
+gem 'mislav-will_paginate', :lib => 'will_paginate'
+gem 'thoughtbot-factory_girl', :lib => 'factory_girl'
+
+if yes?("Do you want to use Hoptoad?")
+  plugin 'hoptoad_notifier', :git => 'git://github.com/thoughtbot/hoptoad_notifier.git', :submodule => true
+  
+  initializer "hoptoad.rb", <<-TXT
+  HoptoadNotifier.configure do |config|
+    config.api_key = "YOUR-API-KEY"
+  end
+  TXT
 end
+
+if yes?("Do you want to install RSpec, Cucumber, Factory_Girl?")
+  run "gem install rspec"
+  run "gem install rspec-rails"
+  run "gem install factory_girl"
+  run "gem install cucumber"
+  run "gem install cucumber-rails"
+end
+
+# Configure Tests
+if yes?("Do you want to use RSpec?")
+  generate :rspec
+  
+  # generate the website controller
+  generate :rspec_controller, "website", "index"
+end
+
+if yes?("Do you want to use Cucumber?")
+  generate :cucumber
+end
+
+# copy sample database config
+run "cp config/database.yml config/database.yml.sample"
+
+# copy some files
+file "config/locales/pt.yml", open("http://github.com/svenfuchs/rails-i18n/raw/master/rails/locale/pt-BR.yml").read
+file "public/javascripts/jquery.js", open("http://code.jquery.com/jquery-1.4.min.js").read
+file "public/javascripts/application.js", <<-TXT
+/*
+  Your application package
+*/
+TXT
 
 generate :nifty_layout
 
-git :init
+git :submodule => "init"
+git :add => "."
+git :commit => %(-m "Bootstrapping #{APP_NAME}")
 
-run "echo 'TODO add readme content' > README"
-run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-run "cp config/database.yml config/example_database.yml"
 
-file ".gitignore", <<-END
-log/*.log
-tmp/**/*
-config/database.yml
-db/*.sqlite3
-END
-
-git :add => ".", :commit => "-m 'initial commit'"
+# ------- # 
--- railscasts.rb
+++ railscasts.rb
@@ -1,10 +1,40 @@
+# HELLO
 run "echo 'Example Railscasts Application' > README"
-run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
-run "rm public/images/rails.png"
-run "rm public/index.html"
 
-file ".gitignore", <<-END
-log/*.log
-tmp/**/*
+# remove all useless public files
+run "rmdir tmp/{pids,sessions,sockets,cache}"
+run "rm README"
+run "rm log/*.log"
+run "rm public/index.html"
+run "rm public/javascripts/*"
+run "rm public/favicon.ico"
+run "rm public/images/rails.png"
+run "rm public/robots.txt"
+run "rm -rf test"
+run "rm -rf doc"
+run "rm config/locales/*"
+
+# keep empty dirs
+run("find . \\( -type d -empty \\) -and \\( -not -regex ./\\.git.* \\) -exec touch {}/.gitkeep \\;")
+
+# basic .gitignore file
+file '.gitignore', 
+%q{log/*.log
+log/*.pid
+db/*.db
 db/*.sqlite3
-END
+db/schema.rb
+tmp/**/*
+.DS_Store
+Thumbs.db
+doc/api
+doc/app
+config/database.yml
+autotest_result.html
+coverage
+public/javascripts/*_packaged.js
+public/stylesheets/*_packaged.css
+public/attachments
+}
+
+generate :nifty_layout
